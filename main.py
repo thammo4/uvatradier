102cb5f3cb79dc1b9dbcf4a76da68dd1552787df
thammo4 <thammo4@uic.edu>
2023-08-28 13:14:58 -0400

    Add bull-call spread wrapper function

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from getuser 					import get_profile;
 from equity_orders 				import equity_market_order, equity_limit_order, equity_stop_order, equity_stop_limit_order;
 from get_positions 				import get_positions;
 from get_option_symbols 		import get_option_symbols, symbol_list_to_df, parse_option_expiries;
-from option_orders 				import option_market_order;
+from option_orders 				import option_market_order, bull_call_spread;
 from get_orders 				import get_orders;
 from get_cancelled_orders 		import rejected_order_by_symbol;
 from cancel_pending_order 		import cancel_pending_order;
--- option_orders.py
+++ option_orders.py
@@ -1,6 +1,38 @@
 from config import *
 
 
+
+
+#
+# Bull-call spread
+#
+
+def bull_call_spread (underlying_symbol, option_symbol_0, quantity_0, option_symbol_1, quantity_1, duration='day'):
+	r = requests.post(
+		url 	= '{}/{}'.format(SANDBOX_URL, ORDER_ENDPOINT),
+		data 	= {
+			'class' 			: 'multileg',
+			'symbol' 			: underlying_symbol,
+			'type' 				: 'market',
+			'duration' 			: duration,
+			'option_symbol[0]' 	: option_symbol_0,
+			'side[0]' 			: 'buy_to_open',
+			'quantity[0]' 		: quantity_0,
+			'option_symbol[1]' 	: option_symbol_1,
+			'side[1]' 			: 'sell_to_open',
+			'quantity[1]' 		: quantity_1
+		},
+		headers = REQUESTS_HEADERS
+	);
+
+	return r.json();
+
+
+
+
+
+
+
 #
 # Order contract market order
 #

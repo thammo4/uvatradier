93f55badf65447766f394c76b665d840685cab37
thammo4 <thammo4@uic.edu>
2023-09-01 11:41:13 -0400

    Add equity and option filter parameters to get_positions

--- get_positions.py
+++ get_positions.py
@@ -1,37 +1,58 @@
 from config import *
 
-def get_positions(symbols=False):
+def get_positions(symbols=False, equities=False, options=False):
 	'''
-	Fetch and return position data from the Tradier Account API.
+	Fetch and filter position data from the Tradier Account API.
 
 	This function makes a GET request to the Tradier Account API to retrieve position
 	information related to a trading account. The API response is expected to be in
 	JSON format, containing details about the positions held in the account.
 
+	Args:
+	    symbols (list, optional): A list of trading symbols (e.g., stock ticker symbols)
+	                              to filter the position data. If provided, only positions
+	                              matching these symbols will be included.
+	    equities (bool, optional): If True, filter the positions to include only equities
+	                              (stocks) with symbols less than 5 characters in length.
+	                              If False, no filtering based on equities will be applied.
+	    options (bool, optional): If True, filter the positions to include only options
+	                              with symbols exceeding 5 characters in length.
+	                              If False, no filtering based on options will be applied.
+
 	Returns:
-		pandas.DataFrame: A DataFrame containing position information.
+	    pandas.DataFrame: A DataFrame containing filtered position information based on
+	                      the specified criteria.
 
 	Note:
-		• Before using this function, make sure to define the necessary constants,
-		  such as SANDBOX_URL, POSITIONS_ENDPOINT, and REQUESTS_HEADERS, with the
-		  appropriate values in a configuration module.
-		• You can optionally provide a list of trading symbols (symbols) to filter the
-		  position data for specific symbols.
+	    • Before using this function, make sure to define the necessary constants,
+	      such as SANDBOX_URL, POSITIONS_ENDPOINT, and REQUESTS_HEADERS, with the
+	      appropriate values in a configuration module.
 
 	Example:
-		position_data = get_positions()
-		print(position_data)
+	    # Retrieve all positions without filtering
+	    all_positions = get_positions()
 
-		# To filter position data for specific symbols:
-		symbols_to_filter = ['AAPL', 'GOOGL']
-		filtered_data = get_positions(symbols_to_filter)
-		print(filtered_data)
+	    # Retrieve positions for specific symbols ('AAPL', 'GOOGL')
+	    specific_positions = get_positions(symbols=['AAPL', 'GOOGL'])
+
+	    # Retrieve only equities
+	    equities_positions = get_positions(equities=True)
+
+	    # Retrieve only options
+	    options_positions = get_positions(options=True)
 	'''
-	r = requests.get(url='{}/{}'.format(SANDBOX_URL, POSITIONS_ENDPOINT), params={}, headers=REQUESTS_HEADERS)
+	r = requests.get(url='{}/{}'.format(SANDBOX_URL, POSITIONS_ENDPOINT), params={}, headers=REQUESTS_HEADERS);
 
-	positions_df = pd.DataFrame(r.json()['positions']['position'])
+	positions_df = pd.DataFrame(r.json()['positions']['position']);
 
 	if symbols:
-		positions_df = positions_df.query('symbol in @symbols')
+	    positions_df = positions_df.query('symbol in @symbols');
 
-	return positions_df
+	if equities:
+	    positions_df = positions_df[positions_df['symbol'].str.len() < 5];
+	    options = False;
+
+	if options:
+	    positions_df = positions_df[positions_df['symbol'].str.len() > 5];
+
+	return positions_df;

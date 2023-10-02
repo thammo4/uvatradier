2558d05f7a1bb2c2e6f09b9a974dca23f4cdd8d2
wahoo-wah <146786950+wahoo-wah@users.noreply.github.com>
2023-10-02 19:42:17 -0400

    Add more examples to tradier_examples.py
    Sample code to:
    • Fetch account portfolio / orders
    • Fetch quote data about stock
    • Place equity order
    • Place options order

--- tradier_examples.py
+++ tradier_examples.py
@@ -13,12 +13,67 @@ ACCOUNT_NUMBER 	= os.getenv('tradier_acct');
 AUTH_TOKEN 		= os.getenv('tradier_token');
 
 
+
+#
+# Instantiate class-based objects
+#
+
 account = Account(ACCOUNT_NUMBER, AUTH_TOKEN);
 quotes 	= Quotes(ACCOUNT_NUMBER, AUTH_TOKEN);
 options = OptionsData(ACCOUNT_NUMBER, AUTH_TOKEN);
 
-
 options_order = OptionsOrder(ACCOUNT_NUMBER, AUTH_TOKEN);
+equity_order = EquityOrder(ACCOUNT_NUMBER, AUTH_TOKEN);
+
+
+
+#
+# Fetch current bar data about Molson Coors Beverage Co ($TAP)
+#
+
+df_tap = quotes.get_quote_day('TAP');
+
+
+#
+# Place market order to buy 5 shares of TAP
+#
+
+equity_order.order(symbol='TAP', side='buy', quantity=5, order_type='market', duration='gtc'); # gtc = good till cancelled
+
+
+
+#
+# Place market order to long 1 put option contract for TAP
+#
+
+# Determine the OCC symbol to transact because will need as input to options order method
+put_contract_symbol = options.get_chain_day(symbol='TAP', strike=60, option_type='put')['symbol'];
+
+
+# Call OptionsOrder.options_order method to execute buy market order
+#
+# Successful order will return JSON
+# {'order': {'id': 8382667, 'status': 'ok', 'partner_id': '3a8bbee1-5184-4ffe-8a0c-294fbad1aee9'}}
+
+options_order.options_order(occ_symbol=put_contract_symbol, order_type='market', underlying='TAP', side='buy_to_open', quantity=1, duration='gtc');
+
+
+
+#
+# View assets currently in portfolio
+#
+
+account.get_positions();
+
+
+
+#
+# Check if there are pending orders
+#
+
+account.get_orders();
+
+
 
 
 

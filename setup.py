6748d0edc6a754d9f6d0c34d293d34eacf831dfd
thammo4 <thammo4@uic.edu>
2024-02-07 17:29:54 -0500

    Update uvatradier to version 0.2.5

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setup(
 	name='uvatradier',
-	version='0.2.4',
+	version='0.2.5',
 	author='tom hammons',
 	description='wahoowa',
 	long_description=long_description,
